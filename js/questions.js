bdf9565a5fcaadbf7ae9e6dc57a7ce891893d2fb
johnlourdusamy <33256343+johnlourdusamy@users.noreply.github.com>
2022-07-18 22:03:47 +0530

    physics
    Physics Lesson 1

---
+++ js/questions.js
@@ -0,0 +1,68 @@
+let jsq_questions = [
+	{
+		"id": 1,
+		"question": "Electric current is a ______quantity",
+		"options": {
+			"a": "derived",
+			"b": "base",
+			"c": "supplementary",
+			"d": "professional"
+		},
+		"answer": "b"
+	},
+	{
+		"id": 2,
+		"question": "SI unit of temperature is ______",
+		"options": {
+			"a": "celsius",
+			"b": "ampere",
+			"c": "fahrenheit",
+			"d": "kelvin"
+		},
+		"answer": "d"
+	},
+	{
+		"id": 3,
+		"question": "Luminous intensity is the intensity of ______",
+		"options": {
+			"a": "IR light",
+			"b": "UV light",
+			"c": "laser light",
+			"d": "visible light"
+		},
+		"answer": "d"
+	},
+	{
+		"id": 4,
+		"question": "Closeness of two or more measured values is called as ______",
+		"options": {
+			"a": "accuracy",
+			"b": "error",
+			"c": "precision",
+			"d": "approximation"
+		},
+		"answer": "c"
+	},
+	{
+		"id": 5,
+		"question": "Which one of the following statement is wrong?",
+		"options": {
+			"a": "Approximation simplifies the calculation.",
+			"b": "Approximation gives the nearest value only.",
+			"c": "Approximation is very useful when little information is available.",
+			"d": "Approximation gives accurate value."
+		},
+		"answer": "d"
+	},
+	{
+		"id": 6,
+		"question": "Which one the following systems of unit is the British System of unit?",
+		"options": {
+			"a": "CGS",
+			"b": "FPS",
+			"c": "MKS",
+			"d": "SI"
+		},
+		"answer": "b"
+	}
+];

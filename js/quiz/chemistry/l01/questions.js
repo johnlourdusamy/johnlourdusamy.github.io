815364218c26ebd423b9e4cf35fe4dec83904d2b
johnlourdusamy <33256343+johnlourdusamy@users.noreply.github.com>
2022-07-28 16:44:08 +0530

    json picking adjusted

---
+++ js/quiz/chemistry/l01/questions.js
@@ -0,0 +1,345 @@
+let jsq_questions = [
+			{
+		"id": 1,
+		"question": "The liquid metal used in thermometers is",
+		"options": {
+			"a": "copper",
+			"b": "mercury",
+			"c": "silver",
+			"d": "gold"
+	},
+			"answer": "b"
+		},
+		{
+		"id": 2,
+		"question": "The pictorial symbol for water given bythe alchemists was",
+		"options": {
+			"a": "Solid angle",
+			"b": "Accuracy",
+			"c": "Temperature",
+			"d": "Plane angle"
+	},
+			"answer": "b"
+	},
+		{
+		"id": 3,
+		"question": "Which one of the following element nameis not derived from planet?",
+		"options": {
+			"a": "Plutonium",
+			"b": "Neptunium",
+			"c": "Uranium",
+			"d": "Mercury"
+	},
+			"answer": "d"
+	},
+		{
+		"id": 4,
+		"question": "Symbol of mercury is",
+		"options": {
+			"a": "Ag",
+			"b": "Hg",
+			"c": "Au",
+			"d": "Pb"
+	},
+			"answer": "b"
+	},
+		{
+		"id": 5,
+		"question": "A form of non-metal which has highductility is ",
+		"options": {
+			"a": "nitrogen",
+			"b": "oxygen",
+			"c": "chlorine",
+			"d": "carbon"
+	},
+			"answer": "d"
+	},
+		{
+		"id": 6,
+		"question": "The property which allows the metals to be hammered into their sheets is___________",
+		"options": {
+			"a": "ductility",
+			"b": "malleability",
+			"c": "conductivity",
+			"d": "kilogram"
+	},
+		"answer": "B"
+	},
+		{
+		"id": 7,
+		"question": "The non-metal which conducts electriccurrent is ",
+		"options": {
+			"a": "carbon",
+			"b": "oxygen",
+			"c": "aluminium",
+			"d": "sulphu"
+	},
+		"answer": "A"
+	},
+		{
+		"id": 8,
+		"question": "____ is a physical quantity that expresses the degree of hotness or coldness of a substance. ",
+		"options": {
+			"a": "Electric current",
+			"b": "Luminous intensity",
+			"c": "Temperature",
+			"d": "none of the above"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 24,
+		"question": "Luminous intensity is measured by a _____ which gives the luminous intensity in terms of candela. ",
+		"options": {
+			"a": "ammeter",
+			"b": "photometer",
+			"c": "voltmeter",
+			"d": "analog clock"
+	},
+		"answer": "b"
+	},
+		{
+		"id": 25,
+		"question": "Scientists modified the clock's mechanism to obtain ______. ",
+		"options": {
+			"a": "estimation",
+			"b": "approximation",
+			"c": "accuracy",
+			"d": "none of the above"
+	},
+		"answer": "c"
+	}, 
+		{
+		"id": 26,
+		"question": "Atomic clocks have an accuracy of one second in every _____ seconds. ",
+		"options": {
+			"a": "10<sup>9</sup>",
+			"b": "10<sup>3</sup>",
+			"c": "10<sup>10</sup>",
+			"d": "10<sup>23</sup>"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 27,
+		"question": "Time difference between two adjacent time zones is ______. ",
+		"options": {
+			"a": "2 hours",
+			"b": "5:30 hours",
+			"c": "1 hour",
+			"d": "24 hours"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 28,
+		"question": "GMT is measured at the longitude of ____ degree. ",
+		"options": {
+			"a": "20",
+			"b": "0",
+			"c": "10",
+			"d": "5"
+	},
+		"answer": "b"
+	},
+		{
+		"id": 29,
+		"question": "The unit of length in FPS system is foot. ",
+		"options": {
+			"a": "True",
+			"b": "False",
+			"c": " ",
+			"d": " "
+	},
+		"answer": "a"
+	},
+		{
+		"id": 30,
+		"question": "The unit of mass in CGS system is gram. ",
+		"options": {
+			"a": "True",
+			"b": "False",
+			"c": " ",
+			"d": " "
+	},
+		"answer": "b"
+	},
+		{
+		"id": 31,
+		"question": "Heat is physical quantity that expresses the degree of hotness or coldness of a substance. ",
+		"options": {
+			"a": "True",
+			"b": "False",
+			"c": " ",
+			"d": " "
+	},
+		"answer": "b"
+	},
+		{
+		"id": 32,
+		"question": "Heat energy removed from a substance will lower its temperature. ",
+		"options": {
+			"a": "True",
+			"b": "False",
+			"c": " ",
+			"d": " "
+	},
+		"answer": "a"
+	},
+		{
+		"id": 33,
+		"question": "Voltmeter is a device used to measure electric current. ",
+		"options": {
+			"a": "True",
+			"b": "False",
+			"c": " ",
+			"d": " "
+	},
+		"answer": "b"
+	},
+		{
+		"id": 34,
+		"question": "pi radian ",
+		"options": {
+			"a": "Mars climate orbiter",
+			"b": "mol",
+			"c": "7",
+			"d": "180 degree"
+	},
+		"answer": "d"
+	},
+		{
+		"id": 35,
+		"question": "Base quantities ",
+		"options": {
+			"a": "Mars climate orbiter",
+			"b": "mol",
+			"c": "7",
+			"d": "180 degree"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 36,
+		"question": "Amount of substance ",
+		"options": {
+			"a": "Mars climate orbiter",
+			"b": "mol",
+			"c": "7",
+			"d": "180 degree"
+	},
+		"answer": "b"
+	},
+		{
+		"id": 37,
+		"question": "Martian climate ",
+		"options": {
+			"a": "Mars climate orbiter",
+			"b": "mol",
+			"c": "7",
+			"d": "180 degree"
+	},
+		"answer": "a"
+	},
+		{
+		"id": 38,
+		"question": "Q/t ",
+		"options": {
+			"a": "Plane angle",
+			"b": "Royal observatory",
+			"c": "Solid angle",
+			"d": "I"
+	},
+		"answer": "d"
+	},
+		{
+		"id": 39,
+		"question": "GMT ",
+		"options": {
+			"a": "Plane angle",
+			"b": "Royal observatory",
+			"c": "Solid angle",
+			"d": "I"
+	},
+		"answer": "b"
+	},
+		{
+		"id": 40,
+		"question": "Two dimensional ",
+		"options": {
+			"a": "Plane angle",
+			"b": "Royal observatory",
+			"c": "Solid angle",
+			"d": "I"
+	},
+		"answer": "a"
+	},
+		{
+		"id": 41,
+		"question": "Three dimensional ",
+		"options": {
+			"a": "Plane angle",
+			"b": "Royal observatory",
+			"c": "Solid angle",
+			"d": "I"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 42,
+		"question": "Heat energy given to a substance will ____ its temperature. ",
+		"options": {
+			"a": "increase",
+			"b": "decrease",
+			"c": "remains same",
+			"d": "none"
+	},
+		"answer": "c"
+	},
+		{
+		"id": 43,
+		"question": "Quartz clock ",
+		"options": {
+			"a": "periodic vibrations",
+			"b": "ampere",
+			"c": "coulomb",
+			"d": "10<sup>9</sup> seconds"
+	},
+		"answer": "d"
+	},
+		{
+		"id": 44,
+		"question": "Atomic clock ",
+		"options": {
+			"a": "periodic vibrations",
+			"b": "ampere",
+			"c": "coulomb",
+			"d": "10<sup>9</sup> seconds"
+	},
+		"answer": "a"
+	},
+		{
+		"id": 45,
+		"question": "Electric current ",
+		"options": {
+			"a": "periodic vibrations",
+			"b": "ampere",
+			"c": "coulomb",
+			"d": "10<sup>9</sup> seconds"
+	},
+		"answer": "b"
+	}, 
+		{
+		"id": 46,
+		"question": "Charge ",
+		"options": {
+			"a": "periodic vibrations",
+			"b": "ampere",
+			"c": "coulomb",
+			"d": "10<sup>9</sup> seconds"
+	},
+		"answer": "c"
+	}
+
+
+];
